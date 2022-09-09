e29aa9ee37b90b9871303e2f637cbc66f1a77694
Fernanda Vitória <fernandavitoria856@gmail.com>
2022-09-09 14:51:03 -0400

    feat: operadores lógicos

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int nota = -1;
+
+       if (nota >= 80) {
+           System.out.println("Graduação A");
+       } else if (nota < 80 && nota >= 70) {
+           System.out.println("Graduação B");
+       }  else if (nota < 70 && nota >= 60) {
+        System.out.println("Gradução C");
+       } else if (nota < 60 && nota >= 50) {
+           System.out.println("Gradução D");
+       } else {
+           System.out.println("Essa nota é inválida");
+      }
     }
 }
